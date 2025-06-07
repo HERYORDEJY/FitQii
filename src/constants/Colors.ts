3afd2f7b98b69c86a9948a1205cdf1d133d8a2e7
HERYORDEJY <heryordejy.dev@gmail.com>
2025-06-07 11:06:20 +0100

    `color constants -added`

---
+++ src/constants/Colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+    primary: "#BBD39D",
+    secondary: "#BBD39D",
+    text: {
+        primary: "#FFFFFF",
+        secondary: "#FFFFFF80",
+        tertiary: "#FFFFFF60",
+    },
+    header: {
+        text: {
+            primary: "#FFFFFF",
+            secondary: "#F5F5F5",
+        }
+    },
+    bg: "#BBD39D",
+}
