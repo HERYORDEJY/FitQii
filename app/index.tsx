6f476be0d68b56f11df9e71b645efa355ae3d7dd
HERYORDEJY <heryordejy.dev@gmail.com>
2025-06-07 12:13:56 +0100

    `fonts config -added`

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import Home from "~/screens/home/Home";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { SplashScreen } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const [loaded, error] = useFonts({
+    light: require("~/assets/fonts/SpaceGrotesk-Light.ttf"),
+    regular: require("~/assets/fonts/SpaceGrotesk-Regular.ttf"),
+    medium: require("~/assets/fonts/SpaceGrotesk-Medium.ttf"),
+    semiBold: require("~/assets/fonts/SpaceGrotesk-SemiBold.ttf"),
+    bold: require("~/assets/fonts/SpaceGrotesk-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return <Home />;
 }
